31a34fd890d74a00df4fa10148746cd74ecbcb91
root <root@Starkk-s.localdomain>
2020-09-16 01:41:29 +0530

    fol_up not working

--- wasabi/fol_up.py
+++ wasabi/fol_up.py
@@ -16,10 +16,30 @@ def file_list(path):
 		for entry in it:
 			if entry.is_file():
 				files.append(entry.name)
-	return files
+	os.chdir('../')
+	return(files)
 
+path='../../file1'
+path_1='~/'
+name_folder_s3='file1'
+folders=folder_list(path)
+wasabi_conf.create_folder(name_folder_s3)
+curr_dir_s3=name_folder_s3
 
-
+for i in folders:
+	files=[]
+	curr_dir_s3=name_folder_s3
+	curr_dir_s3=curr_dir_s3+'/'+i
+	if curr_dir_s3!=name_folder_s3+'/.':
+		wasabi_conf.create_folder(curr_dir_s3)
+	files=file_list(i)
+	for j in files:
+		print('dir = '+i)
+		if curr_dir_s3==name_folder_s3+'/.':
+			curr_dir_s3=name_folder_s3
+		print('file_dir = '+curr_dir_s3+'/'+j,'./'+j)
+		wasabi_conf.send_file(curr_dir_s3+'/'+j,path_1+curr_dir_s3+'/'+j)
+	#wasabi_conf.create_folder()
 
 
 #for i in subfolders_name
